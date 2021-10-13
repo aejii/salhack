3824c93eaacbdf8201da336ab165f4b7744c7f43
mankool <8884398+mankool0@users.noreply.github.com>
2021-10-13 14:40:28 -0700

    Removed non UTF-8 character

--- src/main/java/me/ionar/salhack/util/render/ChatColor.java
+++ src/main/java/me/ionar/salhack/util/render/ChatColor.java
@@ -9,7 +9,7 @@ public enum ChatColor
     BLACK('0'), DARK_BLUE('1'), DARK_GREEN('2'), DARK_AQUA('3'), DARK_RED('4'), DARK_PURPLE('5'), GOLD('6'), GRAY('7'), DARK_GRAY('8'), BLUE('9'), GREEN('a'), AQUA('b'), RED('c'), LIGHT_PURPLE('d'),
     YELLOW('e'), WHITE('f'), MAGIC('k', true), BOLD('l', true), STRIKETHROUGH('m', true), UNDERLINE('n', true), ITALIC('o', true), RESET('r');
 
-    // The typical color character, compiles into '§'
+    // The typical color character
     public static final char COLOR_CHAR = '\u00A7';
 
     // Color character
@@ -57,7 +57,7 @@ public enum ChatColor
     }
 
     /**
-     * Translate '§' into another character, such as '&', '*', etc, for more readability
+     * Translate 'ï¿½' into another character, such as '&', '*', etc, for more readability
      *
      * @param altColorChar    the new color code character
      * @param textToTranslate the string to translate, could be '&cHyperium'
